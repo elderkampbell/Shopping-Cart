9110615e206b4772bd74a15c47fa5980b2d5da45
Elder Kampbell <elderkampbell@hotmail.com>
2022-08-09 16:22:14 -0300

    Requisito 1

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,9 +6,22 @@ describe('1 - Teste a função fetchProducts', () => {
   test('Testa se fetchProducts é uma função', () => {
     expect(typeof fetchProducts).toBe('function');
   });
-  test('Testa se a função fetch foi chamada ao executar |fetchProducts| com o argumento |computador|', async () => {
-    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
-    fetchProducts('computador');    
-    expect(fetch).toBeCalledWith(url)
+  test('Testa se a função fetch foi chamada ao executar fetchProducts', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalled();
+  });
+  test('Testa se executando fetchProducts com o argumento computador, fetch utiliza determinado endpoint', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toBeCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+  });
+  test('Testa se o retorno da função fetchProducts com o argumento computador é igual a computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toBeEqual(computadorSearch)
+  });
+  test('', async () => {
+    try {
+      await fetchProducts();
+    } catch (error) {
+      expect(error).toEqual(new Error('You must provide an url'))
+    }
   });
 });
