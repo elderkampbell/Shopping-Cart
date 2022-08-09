5c673e4ccc0b8682e807dc0cb23559ddb0ab0af6
Elder Kampbell <elderkampbell@hotmail.com>
2022-08-09 15:42:22 -0300

    Iniciando o Projeto

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,12 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Testa se fetchProducts é uma função', () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  test('Testa se a função fetch foi chamada ao executar |fetchProducts| com o argumento |computador|', async () => {
+    const url = 'https://api.mercadolibre.com/sites/MLB/search?q=computador'
+    fetchProducts('computador');    
+    expect(fetch).toBeCalledWith(url)
+  });
 });
